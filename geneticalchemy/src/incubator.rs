dfec8688302672f6460deec5a452e029f09b242e
Natalia Borovskikh <lai.tash@gmail.com>
2022-12-12 12:43:22 -0300

    add more information to alchemical phenotype

--- geneticalchemy/src/incubator.rs
+++ geneticalchemy/src/incubator.rs
@@ -6,6 +6,15 @@ pub use grimoire::prelude::*;
 
 
 #[derive(Serialize, Clone, Debug)]
+pub struct AlchemyEffectTotal {
+    direct: f64,
+    over_time: f64,
+    per_second: f64,
+    length: f64,
+}
+
+
+#[derive(Serialize, Clone, Debug)]
 pub struct AlchemyPhenotypeEffects {
     volume: f64,
     dh: f64,
@@ -21,6 +30,8 @@ pub struct AlchemyPhenotypeEffects {
 #[derive(Serialize, Clone, Debug)]
 pub struct AlchemyPhenotype {
     effects: AlchemyPhenotypeEffects,
+    healing: AlchemyEffectTotal,
+    poison: AlchemyEffectTotal,
     ingredients: Vec<(String, u64)>
 }
 
@@ -58,9 +69,24 @@ impl Incubator for AlchemyIncubator {
             pl: mix_effect(&mix, Property::PoisonLength),
             a: mix_effect(&mix, Property::Alcohol),
         };
+
+        let healing = AlchemyEffectTotal {
+            direct: effects.dh,
+            per_second: effects.hot * effects.volume.sqrt(),
+            over_time: effects.hot * effects.hl * effects.volume,
+            length: effects.hl * effects.volume.sqrt(),
+        };
+
+        let poison = AlchemyEffectTotal {
+            direct: effects.dp,
+            per_second: effects.pot * effects.volume.sqrt(),
+            over_time: effects.pot * effects.pl * effects.volume,
+            length: effects.hl * effects.volume.sqrt(),
+        };
+
         let ingredients = mix.ingredients.iter().map(|x| (x.0.name.clone(), x.1)).collect();
         AlchemyPhenotype {
-            effects, ingredients
+            effects, ingredients, healing, poison
         }
     }
 }
