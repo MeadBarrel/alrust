2da75ccfa08ca2f9c5bc3382719863ead5911109
Natalia Borovskikh <lai.tash@gmail.com>
2022-12-19 11:22:19 -0300

    Change CharacterUpdate to use a chain of commands

--- grimoire2/src/modify/character.rs
+++ grimoire2/src/modify/character.rs
@@ -1,5 +1,3 @@
-use std::collections::HashSet;
-
 use serde::{Serialize, Deserialize};
 
 use crate::{grimoire::Character};
@@ -12,12 +10,16 @@ pub enum ModifyClade {
 }
 
 
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub enum CharacterUpdateCommand {
+    AddClade(String),
+    RemoveClade(String),
+    SetSkill(String, u8)
+}
+
 #[derive(Default, Clone, Debug, Serialize, Deserialize)]
 pub struct CharacterUpdate {
-    pub clades_add: Vec<String>,
-    pub clades_remove: HashSet<String>,
-    pub skills: Vec<(String, u8)>,
-    pub skills_remove: HashSet<String>,
+    commands: Vec<CharacterUpdateCommand>,
 }
 
 impl CharacterUpdate {
@@ -37,29 +39,39 @@ impl CharacterUpdate {
     }
 
     pub fn update(&self, character: &mut Character) {
-        character.clades.retain(|x| !self.clades_remove.contains(x));
-        character.skills.retain(|x, _| !self.skills_remove.contains(x));
-        character.clades.extend(self.clades_add.iter().cloned());
-        character.skills.extend(self.skills.iter().cloned())
+        self.commands.iter().for_each(|command|
+            match command {
+                CharacterUpdateCommand::AddClade(clade) => { character.clades.insert(clade.clone()); },
+                CharacterUpdateCommand::RemoveClade(clade) => { character.clades.remove(clade.as_str()); },
+                CharacterUpdateCommand::SetSkill(skill, value) => match value {
+                    0 => { character.skills.remove(skill.as_str()); },
+                    x => { 
+                        let v = character.skills.entry(skill.clone()).or_default();
+                        *v = *x;
+                    }
+                }
+            }
+        );
+
     }
 
     pub fn add_clade(&mut self, clade: &str) -> &mut Self {
-        self.clades_add.push(clade.to_string());
+        self.commands.push(CharacterUpdateCommand::AddClade(clade.to_string()));
         self
     }
 
     pub fn remove_clade(&mut self, clade: &str) -> &mut Self {
-        self.clades_remove.insert(clade.to_string());
+        self.commands.push(CharacterUpdateCommand::RemoveClade(clade.to_string()));
         self
     }
 
     pub fn set_skill(&mut self, skill: &str, value: u8) -> &mut Self {
-        self.skills.push((skill.to_string(), value));
+        self.commands.push(CharacterUpdateCommand::SetSkill(skill.to_string(), value));
         self
     }
 
     pub fn remove_skill(&mut self, skill: &str) -> &mut Self {
-        self.skills_remove.insert(skill.to_string());
+        self.commands.push(CharacterUpdateCommand::SetSkill(skill.to_string(), 0));
         self
     }
 }
@@ -113,16 +125,16 @@ mod tests {
             .add_clade("d")
             .remove_clade("a")
             .remove_clade("b")
-            .remove_clade("d")
             .set_skill("a", 10)
             .set_skill("c", 80)
             .clone();
         
         update.update(&mut character);
 
-        assert!( character.has_clade("a") );
-        assert!( character.has_clade("d"));
+        assert!( !character.has_clade("a") );
         assert!( !character.has_clade("b") );
+        assert!( character.has_clade("d"));
+        
 
         assert_eq!( character.raw_skill("a"), 10 );
         assert_eq!( character.raw_skill("c"), 80 )
