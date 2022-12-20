c387beebaa3d86cefe8ae1e7e918152dd8aa0fa7
Natalia Borovskikh <lai.tash@gmail.com>
2022-12-20 19:38:43 -0300

    WIP

--- src/editor/characters.rs
+++ src/editor/characters.rs
@@ -5,13 +5,18 @@ use grimoire2::grimoire::Grimoire;
 
 #[derive(Debug, Clone, Default)]
 pub struct State {
+    id: Option<usize>,
     creating_characters: Vec<(egui::Id, String)>,
     editing_characters: Vec<(egui::Id, String)>,
 }
 
 
 pub fn editor(ui: &mut Ui, wishes: &mut Wishes, state: &mut State, grimoire: &mut Grimoire) {
-    egui::TopBottomPanel::top("character_editor_tb").show(ui.ctx(), |ui| {
+    if state.id.is_none() { state.id = Some(wishes.counter()) }
+
+    let top_panel_id = egui::Id::new(format!("character_editor_tb_{}", state.id.unwrap()));
+
+    egui::TopBottomPanel::top(top_panel_id).show(ui.ctx(), |ui| {
         top_panel(ui, wishes, state);
     });
 
@@ -23,33 +28,32 @@ pub fn editor(ui: &mut Ui, wishes: &mut Wishes, state: &mut State, grimoire: &mu
             .id(*id)
             .open(&mut window_close_not_clicked)
             .show(ui.ctx(), |ui| {
-                ui.vertical(|ui| {
-                    let text_edit_has_focus = ui.text_edit_singleline(name).lost_focus();
-                    if text_edit_has_focus && ui.ctx().input().key_pressed(egui::Key::Escape) {
-                        open = false;
-                    }        
-
-                    ui.horizontal(|ui| {
-                        if ui.button("Add").clicked() {};
-                        if ui.button("Cancel").clicked() {
-                            open = false;
-                        };    
-                    })
-                })
+                open = create_character_window(ui, name)
             });
             
             window_close_not_clicked && open
     })
 
-    // for (id, name) in &mut state.creating_characters {
-    //     let mut open = true;
-    //     egui::Window::new("Create new Character")
-    //         .id(*id)
-    //         .open(&mut open)
-    //         .show(ui.ctx(), |ui| {
-    //         ui.text_edit_singleline(name);
-    //     });
-    // }
+}
+
+
+fn create_character_window(ui: &mut Ui, name: &mut String) -> bool {
+    let mut open = true;
+    ui.vertical(|ui| {
+        let text_edit_has_focus = ui.text_edit_singleline(name).lost_focus();
+        if text_edit_has_focus && ui.ctx().input().key_pressed(egui::Key::Escape) {
+            open = false;
+        }        
+
+        ui.horizontal(|ui| {
+            if ui.button("Add").clicked() {};
+            if ui.button("Cancel").clicked() {
+                open = false;
+            };    
+        })
+    });
+    
+    open
 }
 
 
