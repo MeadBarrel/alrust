a69ee2241a39266cc33a5ce4c2ba67b9ee8e5847
Natalia Borovskikh <lai.tash@gmail.com>
2022-12-24 16:29:13 -0300

    That's it, no tauri

--- grimoire-yew/dist/snippets/grimoire-yew-bf0451a021b18512/public/glue.js
+++
@@ -1,8 +0,0 @@
-const invoke = window.__TAURI__.invoke
-
-export async function invoke_open_grimoire_dialog() {
-    console.log("FUCKL!");
-    var result = await invoke("open_grimoire_dialog", {});
-    console.log(result);
-    return result;
-}
