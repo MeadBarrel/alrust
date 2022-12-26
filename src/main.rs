220faa7bc0efd9ee4af4077b8c5226beecd080fd
Natalia Borovskikh <lai.tash@gmail.com>
2022-12-26 18:10:25 -0300

    help

--- src/main.rs
+++ src/main.rs
@@ -22,6 +22,41 @@ pub fn main() {
                 .index(1)
                 .value_name("from")
                 .required(true)
+                .help("Grimoire update file")
+                .long_help(
+                    "Grimoire update file\n\
+                     ====================\n\
+                    Format of the file:\n\n\
+                    remove_characters:\n\t<name>\n\t...\n\n\
+                    remove_skills:\n\t<name>\n\t...\n\n\
+                    remove_ingredients:\n\t<name>\n\t...\n\n\
+                    characters:\n\
+                    \t<character name>:\n\
+                    \t\tremove_clades:\n\t\t\t- <clade>\n\t\t\t...\n\n\
+                    \t\tremove_skills:\n\t\t\t- <skill>\n\t\t\t...\n\n\
+                    \t\tadd_clades:\n\t\t\t- <clade>\n\t\t\t...\n\n\
+                    \t\tskills:\n\t\t\t<skill>:<value>\n\t\t\t...\n\n\
+                    skills:\n\
+                    \t<skill name>:\n\
+                    \t\teffectiveness: (effectiveness, theoretical*)\n\
+                    \t\tparent: <name of parent 1>\n\
+                    \t\tparent_2: <name of parent 2>\n\
+                    \t\tremove_parent: bool\n\
+                    \t\tremove_parent_2: bool\n\n\
+                    ingredients:\n\
+                    \t\t<name of ingredient>:\n\
+                    \t\t\tskill: <lore of ingredient>\n\
+                    \t\t\tremove_skill: bool  # remove lore\n\
+                    \t\t\tweight: bool  # whether the ingredient has alchemical weight\n\
+                    \t\t\tdh: (direct healing, theoretical*)\n\
+                    \t\t\tmdh: (direct healing multiplier, theoretical*)\n\
+                    \t\t\t<... dp, mdp, hot, mhot, pot, mpot, hl, mhl, pl, mpl, a, ma>\
+                    \n\n\
+                    * theoretical format is either:\n\
+                    \t<floating point value (5, 0.4, 3.1 etc)> - Known value\n\
+                    \t!?<floating point value> - Theoretical value\n\
+                    \t?? - Value is unknown" 
+                )
         )
         .arg(
             Arg::new("to")
